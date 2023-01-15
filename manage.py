e1bf8fcbfb730cb5b80d0dd28fc721f3653c439f
Anshuman Tiwari <112653339+AnshumanCse@users.noreply.github.com>
2023-01-15 17:01:38 +0530

    Add version

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
-import sys
+import sys,
+import version;
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "extra_views_tests.settings")
